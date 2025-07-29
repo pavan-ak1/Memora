b9438420cb5a7473c8c0b70ae55355e569f60c60
pavan-ak1 <kustagipavan30@gmail.com>
2025-07-29 14:08:44 +0530

    Added URLs

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = "https://memora-hmyj.onrender.com"
 
-export const FRONTEND_URL = "http://localhost:5173"
+export const FRONTEND_URL = "https://memora-psi.vercel.app/"
